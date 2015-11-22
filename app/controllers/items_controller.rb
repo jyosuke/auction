664333245a3994fc2b5d04859e76f1bcffd12321
jyosuke <kikuchikun@icloud.com>
2015-11-22 14:13:30 +0900

    new作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,9 @@ class ItemsController < ApplicationController
     #Itemというモデルに対してfindメソッドでidを検索させている
     @item = Item.find(params[:id])
   end
-
+  def new
+  @item = Item.new
+  end
   def index
   @items = Item.all
   end
